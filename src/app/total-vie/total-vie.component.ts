0c529d566d1549d11fe5e50d9272fc2c7a2fde3a
duonglong753 <64320868+duonglong753@users.noreply.github.com>
2022-04-17 14:56:45 +0700

    fix typo in the heat map data

--- src/app/total-vie/total-vie.component.ts
+++ src/app/total-vie/total-vie.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit,ViewChild  } from '@angular/core';
 import * as Highcharts from "highcharts/highmaps";
-const worldMap = require('@highcharts/map-collection/countries/vn/vn-all.topo.json');
+const worldMap = require('./js/vn-all.json');
 @Component({
   selector: 'app-total-vie',
   templateUrl: './total-vie.component.html',
@@ -62,20 +62,20 @@ chartOptions: any= {
        ['vn-3655', 10], ['vn-qn', 11], ['vn-kh', 12], ['vn-tg', 13],
         ['vn-bv', 14], ['vn-bu', 15], ['vn-hc', 16], ['vn-br', 17],
         ['vn-st', 18], ['vn-pt', 19], ['vn-yb', 20], ['vn-hd', 21],
-        ['vn-bn', 22], ['vn-317', 23], ['vn-nb', 24], ['vn-hm', 25],
-        ['vn-ho', 26], ['vn-vc', 27], ['vn-318', 28], ['vn-bg', 29],
+        ['vn-bn', 22], ['vn-hy', 23], ['vn-nb', 24], ['vn-hm', 25],
+        ['vn-ho', 26], ['vn-vc', 27], ['vn-hn', 28], ['vn-bg', 29],
         ['vn-tb', 30], ['vn-ld', 31], ['vn-bp', 32], ['vn-py', 33],
-        ['vn-bd', 34], ['vn-724', 35], ['vn-qg', 36], ['vn-331', 37],
-        ['vn-dt', 38], ['vn-la', 39], ['vn-3623', 40], ['vn-337', 41],
+        ['vn-bd', 34], ['vn-gl', 35], ['vn-qg', 36], ['vn-dn', 37],
+        ['vn-dt', 38], ['vn-la', 39], ['vn-hp', 40], ['vn-hag', 300],
         ['vn-bl', 42], ['vn-vl', 43], ['vn-tn', 44], ['vn-ty', 45],
-        ['vn-li', 46], ['vn-311', 47], ['vn-hg', 48], ['vn-nd', 49],
-        ['vn-328', 50], ['vn-na', 51], ['vn-qb', 52], ['vn-723', 53],
-        ['vn-nt', 54], ['vn-6365', 55], ['vn-299', 56], ['vn-300', 57],
+        ['vn-li', 46], ['vn-sl', 47], ['vn-hg', 48], ['vn-nd', 49],
+        ['vn-ht', 50], ['vn-na', 51], ['vn-qb', 52], ['vn-dlk', 53],
+        ['vn-nt', 54], ['vn-dkn', 55], ['vn-kt', 56], ['vn-qnam', 57],
         ['vn-qt', 58], ['vn-tt', 59], ['vn-da', 60], ['vn-ag', 61],
         ['vn-cm', 62], ['vn-tv', 63], ['vn-cb', 64], ['vn-kg', 65],
-        ['vn-lo', 66], ['vn-db', 67], ['vn-ls', 68], ['vn-th', 69],
-        ['vn-307', 70], ['vn-tq', 71], ['vn-bi', 72], ['vn-333', 73]
-        
+        ['vn-lo', 66], ['vn-db', 67], ['vn-ls', 100], ['vn-th', 69],
+        ['vn-bk', 70], ['vn-tq', 71], ['vn-bi', 72], ['vn-ct', 73]
+
       ]
     }
   ]
